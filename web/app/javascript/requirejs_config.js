84596a4ae34c5d4e7877f7d58f0ebd08f88ada03
sanyiwangshan <13823154579@163.com>
2015-10-31 14:10:39 +0800

    引入文件样式

--- web/app/javascript/requirejs_config.js
+++ web/app/javascript/requirejs_config.js
@@ -2,11 +2,11 @@ require.config({
   paths: {
     "jquery": "bower_components/jquery/dist/jquery",
     "angular": "bower_components/angular/angular",
-    "unzip": "node_modules/unzip/unzip",
-    "archiver": "node_modules/archiver/index"
+    "pace": "app/javascript/pace.min",
+    "endless": "app/javascript/endless"
   },
   shim: {
-    "angular": {
+    "endless": {
       deps: ["jquery"]
     }
   }
