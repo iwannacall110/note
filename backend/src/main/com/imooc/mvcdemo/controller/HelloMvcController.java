5ccc5b8274669587b813ee15a39b10c746c0215d
sanyiwangshan <13823154579@163.com>
2015-11-07 09:42:25 +0800

    no message

--- backend/src/main/com/imooc/mvcdemo/controller/HelloMvcController.java
+++ backend/src/main/com/imooc/mvcdemo/controller/HelloMvcController.java
@@ -1,13 +1,16 @@
 package com.imooc.mvcdemo.controller;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hello")
-public class HelloMvcController {
+public class HelloMvcController {	
+	private static org.apache.log4j.Logger log = LoggerFactory.getLoger(HelloMvcController.class)
 	@RequestMapping("/mvc")
 	public String helloMvc(){
+		log.debug("========================hello, world!");
 		return "home";
 	}
 }
