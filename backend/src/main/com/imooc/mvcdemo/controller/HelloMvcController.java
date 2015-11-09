7f5ea5a1b179e10ca3560fcb9d11aec92c659ba5
sanyiwangshan <13823154579@163.com>
2015-11-09 18:58:46 +0800

    修改文件

--- backend/src/main/com/imooc/mvcdemo/controller/HelloMvcController.java
+++ backend/src/main/com/imooc/mvcdemo/controller/HelloMvcController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/test")
 public class HelloMvcController {
+	Logger logger = Logger.getLogger(HelloMvcController.class)
 	@RequestMapping("/mvc")
 	public String helloMvc(){
+		logger.debug("===================================");
 		return "home";
 	}
 }
