bcbc138ec40fe347e98bc913dba3ee3497e37fbc
sanyiwangshan <13823154579@163.com>
2016-01-09 18:50:36 +0800

    css

--- web/app/javascript/angular_bootstrap.js
+++ web/app/javascript/angular_bootstrap.js
@@ -1,4 +1,3 @@
-//当DOM结构加载完成后，angular_bootstrap.js文件将会命令AngularJS启动起来并继续执行
 define(['angular', 'domReady', 'app'], function(angular, domReady) {
     require(['domReady!'], function (document) {
         angular.bootstrap(document, ['webapp']);
