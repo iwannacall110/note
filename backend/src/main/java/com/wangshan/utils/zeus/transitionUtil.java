12ed2417b146fee4b049f381154add8abcb947b9
sanyiwangshan <13823154579@163.com>
2016-01-20 18:34:59 +0800

    去掉之前无用的eclipse配置文件

--- backend/src/main/java/com/wangshan/utils/zeus/transitionUtil.java
+++ backend/src/main/java/com/wangshan/utils/zeus/transitionUtil.java
@@ -1,32 +1,40 @@
 package com.wangshan.utils.zeus;
 
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+import java.io.BufferedReader;
+import java.sql.Blob;
+import java.sql.SQLException;
+
 /**
  * Created by shan on 2016/1/18.
  */
 
 public class TransitionUtil {
-
+/*
     public static String blobToStr(Blob blob){
-        String result =""
-        try{
-            return new String(blob.getBytes((long)1, (int)blob.length()));
-        } catch(SQLException e) {
+        String result = "";
+        try {
+            result = new String(blob.getBytes((long)1, (int)blob.length()));
+        } catch(SQLException e){
             e.printStackTrace();
         }
+        return result;
     }
 
-    public static  Blob strToBlob(String str){
-        BufferedReader BlobStream = new BufferedReader(Blob.getCharacterStream());
-        StringBuffer stringBuffer = new StringBuffer();
-        int nchars = 0;
-        char[] buffer = new char[10];
-        while((nchars = BlobStream.read(buffer)) != -1 )
-            stringBuffer.append(buffer, 0, nchars);
-        BlobStream.close();
-        return Util.strTrim(stringBuffer.toString());
+    public static SerialBlob strToBlob(String str){
+		SerialBlob blob = null;
+        try {
+            blob = new SerialBlob(str.getBytes());
+        } catch(SerialException e){
+            e.printStackTrace();
+        }
+        return blob;
     }
 
-    public static void main(args[] String){
-        System.out.println("================================")
+    public static void main(String[] args){
+
+        System.out.println("================================");
     }
+	*/
 }
