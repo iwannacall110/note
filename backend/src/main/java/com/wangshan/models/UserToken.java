ae47d45c8c3ae389ec35f5e1ac38c27a075e1760
sanyiwangshan <13823154579@163.com>
2016-02-19 18:05:56 +0800

    update

---
+++ backend/src/main/java/com/wangshan/models/UserToken.java
@@ -0,0 +1,73 @@
+package com.wangshan.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Administrator on 2015/11/15.
+ */
+
+public class UserToken {
+    private Long user;
+    private String token;
+    private Integer state;
+    private Timestamp createon;
+    private Timestamp updateon;
+
+    public UserToken(){}
+
+    public UserToken(Long user, String token, Integer state, DateTime createon, DateTime updateon){
+        this.user = user;
+        this.token = token;
+        this.state = state;
+        this.createon = createon.getMillis;
+        this.updateon = updateon;
+    }
+
+    public Long getUser() {
+        return user;
+    }
+
+    public void setUser(Long user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    public Timestamp getCreateon() {
+        return createon;
+    }
+
+    public void setCreateon(Timestamp createon) {
+        this.createon = createon;
+    }
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    public Timestamp getUpdateon() {
+        return updateon;
+    }
+
+    public void setUpdateon(Timestamp updateon) {
+        this.updateon = updateon;
+    }
+}
